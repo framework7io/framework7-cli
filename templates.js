dbfcd46e4fab319ae6c443928c126c0ac1f47522
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-02-27 23:32:42 +0300

    Templates spec

---
+++ templates.js
@@ -0,0 +1,4 @@
+module.exports = {
+    singleview: 'https://github.com/nolimits4web/framework7-cli-template-singleview',
+    tabs: 'https://github.com/nolimits4web/framework7-cli-template-tabs'
+};
