dfcad4585e8b995bcbc4ff1061865850375869ca
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-02-28 17:46:06 +0300

    Colorize output

--- utils/log.js
+++ utils/log.js
@@ -1,13 +1,19 @@
 /* =============================================
     Logs
 ============================================= */ 
+var chalk = require('chalk');
 module.exports = {
-    text: function (text) {
+    text: function (text, color, bold) {
         if (text === '\n' || text === '\r\n' || text === '\r' || text === '' || text.trim && text.trim() === '') return;
-        console.log(text);
+        if (color) {
+            console.log(chalk[color](text));
+        }
+        else {
+            console.log(text);
+        }
     },
     error: function (text) {
         if (text === '\n' || text === '\r\n' || text === '\r' || text === '' || text.trim && text.trim() === '') return;
-        console.log(text);
+        console.error(chalk.red(text));
     }
 };
