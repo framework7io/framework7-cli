d3101db06a2dc5b3f2107fdf7acc1c7e4bb07ac1
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-02-28 17:45:46 +0300

    Rename templates to JSON

--- templates.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    singleview: 'https://github.com/nolimits4web/framework7-cli-template-singleview',
-    tabs: 'https://github.com/nolimits4web/framework7-cli-template-tabs'
-};
