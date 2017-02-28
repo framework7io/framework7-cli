23818e7306f3313f671a57c5d94f3427bf8a451f
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-02-28 17:07:07 +0300

    Don't output empty lines

--- utils/log.js
+++ utils/log.js
@@ -2,10 +2,12 @@
     Logs
 ============================================= */ 
 module.exports = {
-    text: function text (text) {
+    text: function (text) {
+        if (text === '\n' || text === '\r\n' || text === '\r' || text === '' || text.trim && text.trim() === '') return;
         console.log(text);
     },
-    error: function error (text) {
+    error: function (text) {
+        if (text === '\n' || text === '\r\n' || text === '\r' || text === '' || text.trim && text.trim() === '') return;
         console.log(text);
     }
 };
