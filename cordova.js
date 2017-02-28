00c6d1bc61977a3be63b21307052185ff583981a
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-02-28 17:07:29 +0300

    Lots of refactoring

--- cordova.js
+++ cordova.js
@@ -1,13 +1,16 @@
 /* =============================================
     Cordova Wrapper
 ============================================= */ 
+var path = require('path');
 var exec = require('./utils/exec.js');
 var log = require('./utils/log.js');
 var fs = require('fs');
-var xml2js = require('xml2js');
+var fse = require('fs-extra');
 var templates = require('./templates.js');
-var ghdownload = require('github-download');
 var download = require('./utils/download.js');
+var downloadFramework7 = require('./utils/download-framework7.js');
+var downloadGithub = require('./utils/download-github.js');
+var configXml = require('./utils/config-xml.js');
 
 module.exports = {
     // -----> Cordova Create
@@ -30,146 +33,58 @@ module.exports = {
         var cmd = `cordova create ${[folder, id, name].join(' ')}`;
         exec(cmd, function () {
             // Clear www folder
-            exec('rm -rf ' + folder + '/www');
-            // Create empty folders
-            exec('mkdir ' + folder + '/src');
-            exec('mkdir ' + folder + '/www');
-            // Paths
-            var projectRoot = process.cwd() + '/' + folder;
-            var projectSrc = process.cwd() + '/' + folder + '/src/';
-            // Modify XML
-            var xmlPath = projectRoot + '/config.xml';
-            var xmlContent = fs.readFileSync(xmlPath);
-            xml2js.parseString(xmlContent, function (error, result) {
-                result.widget.preference = [];
-                [['webviewbounce', 'false'], ['UIWebViewBounce', 'false'], ['DisallowOverscroll', 'true']].forEach(function (el) {
-                    result.widget.preference.push({
-                        '$': {
-                            name: el[0],
-                            value: el[1]
-                        }
+            var projectRoot = path.join(process.cwd(), folder);
+            var projectTmp = path.join(projectRoot, 'tmp');
+            fse.emptyDirSync(path.join(projectRoot, 'www'));
+            // Create src folder
+            fse.ensureDirSync(path.join(projectRoot, 'src'));
+            // Create tmp folder
+            fse.ensureDirSync(projectTmp);
+            // Download template from GitHub
+            log.text('Downloading template');
+            downloadGithub(templates[template], projectTmp, function (error) {
+                if (error) return log.error(error);
+                log.text('Template OK');
+                // Read template manifest
+                var manifest = require(path.join(projectTmp, 'manifest.json'));
+                var projectSrc = path.join(projectRoot, manifest.src);
+                // Modify XML
+                if (manifest.xml) {
+                    log.text('Modify config.xml');
+                    configXml(path.join(projectRoot, 'config.xml'), manifest.xml, function (error) {
+                        if (error) return log.error(error);
+                        log.text('Modify config.xml OK');
+                    });
+                }
+                // Check for scripts
+                if (manifest.scripts) {
+                    if (Array.isArray(manifest.scripts)) {
+                        manifest.scripts.forEach(function (script) {
+                            fse.copySync(path.join(projectTmp, script), path.join(projectRoot, script));
+                            fse.removeSync(path.join(projectTmp, script));
+                        });
+                    }
+                    else {
+                        fse.copySync(path.join(projectTmp, manifest.scripts), path.join(projectRoot, 'scripts'));
+                        fse.removeSync(path.join(projectTmp, manifest.scripts));
+                    }
+                }
+                // Copy folder
+                fse.copy(projectTmp, projectSrc, {overwrite: true}, function (error) {
+                    if (error) return log.error(error);
+                    // Remove Temp Folder
+                    fse.removeSync(projectTmp);
+                    // Download latest version of Framework7
+                    log.text('Downloading latest version of Framework7 and icon fonts');
+                    var cssPath = manifest.css ? path.join(projectSrc, manifest.css) : undefined;
+                    var jsPath = manifest.css ? path.join(projectSrc, manifest.js) : undefined;
+                    var fontsPath = manifest.css ? path.join(projectSrc, manifest.fonts) : undefined;
+                    downloadFramework7(cssPath, jsPath, fontsPath, function (error) {
+                        if (error) return log.error(error);
+                        log.text('Downloading latest version of Framework7 and icon fonts OK');
                     });
                 });
-                var newXmlContent = (new xml2js.Builder()).buildObject(result);
-                fs.writeFileSync(xmlPath, newXmlContent);
             });
-            // Download Template
-            log.text('Downloading template');
-            ghdownload(templates[template], projectSrc)
-                .on('dir', function (dir) {
-                    // console.log('dir', dir);
-                })
-                .on('file', function (file) {
-                    // console.log('file', file);
-                })
-                .on('zip', function (zip) {
-                    // console.log('zip', zip);
-                })
-                .on('error', function (error) {
-                    log.error('Error while downloading template: ' + error);
-                })
-                .on('end', function () {
-                    // Download latest version of Framework7
-                    log.text('Template OK');
-                    log.text('Downloading latest version of Framework7 and icon fonts');
-                    // Read manifest
-                    var manifest = require(projectSrc + '/manifest.json');
-                    
-                    var cssFiles = [
-                        'framework7.ios.min.css',
-                        'framework7.ios.colors.min.css',
-                        'framework7.ios.rtl.min.css',
-                        'framework7.material.min.css',
-                        'framework7.material.colors.min.css',
-                        'framework7.material.rtl.min.css'
-                    ];
-                    var jsFiles = [
-                        'framework7.min.js'
-                    ];
-                    var f7FontFiles = [
-                        'Framework7Icons-Regular.eot',
-                        'Framework7Icons-Regular.ttf',
-                        'Framework7Icons-Regular.woff',
-                        'Framework7Icons-Regular.woff2'
-                    ];
-                    var materialFontFiles = [
-                        'MaterialIcons-Regular.eot',
-                        'MaterialIcons-Regular.ttf',
-                        'MaterialIcons-Regular.woff',
-                        'MaterialIcons-Regular.woff2'
-                    ];
-                    var newFile, request;
-                    if (manifest.css) {
-                        cssFiles.forEach(function (filename) {
-                            download(
-                                'https://raw.githubusercontent.com/nolimits4web/Framework7/master/dist/css/' + filename, 
-                                projectSrc + manifest.css + filename,
-                                function (error) {
-                                    if (error) log.error(error);
-                                    else log.text(filename + ' OK');
-                                }
-                            );
-                        });
-                    }
-                    if (manifest.js) {
-                        jsFiles.forEach(function (filename) {
-                            download(
-                                'https://raw.githubusercontent.com/nolimits4web/Framework7/master/dist/js/' + filename, 
-                                projectSrc + manifest.js + filename,
-                                function (error) {
-                                    if (error) log.error(error);
-                                    else log.text(filename + ' OK');
-                                }
-                            );
-                        });
-                    }
-                    if (manifest.fonts) {
-                        f7FontFiles.forEach(function (filename) {
-                            download(
-                                'https://raw.githubusercontent.com/nolimits4web/Framework7-Icons/master/fonts/' + filename, 
-                                projectSrc + manifest.fonts + filename,
-                                function (error) {
-                                    if (error) log.error(error);
-                                    else log.text(filename + ' OK');
-                                }
-                            );
-                        });
-                        download(
-                            'https://raw.githubusercontent.com/nolimits4web/Framework7-Icons/master/css/framework7-icons.css', 
-                            projectSrc + manifest.css + 'framework7-icons.css',
-                            function (error) {
-                                if (error) log.error(error);
-                                else {
-                                    log.text('framework7-icons.css' + ' OK');
-                                }
-                            }
-                        );
-                        materialFontFiles.forEach(function (filename) {
-                            download(
-                                'https://raw.githubusercontent.com/google/material-design-icons/master/iconfont/' + filename, 
-                                projectSrc + manifest.fonts + filename,
-                                function (error) {
-                                    if (error) log.error(error);
-                                    else log.text(filename + ' OK');
-                                }
-                            );
-                        });
-                        download(
-                            'https://raw.githubusercontent.com/google/material-design-icons/master/iconfont/material-icons.css', 
-                            projectSrc + manifest.css + 'material-icons.css',
-                            function (error) {
-                                if (error) log.error(error);
-                                else {
-                                    var materialIconsCSS = fs.readFileSync(projectSrc + manifest.css + 'material-icons.css', 'utf-8');
-                                    materialIconsCSS = materialIconsCSS.replace(/url\(MaterialIcons/g, 'url(../fonts/MaterialIcons');
-                                    fs.writeFileSync(projectSrc + manifest.css + 'material-icons.css', materialIconsCSS);
-                                    log.text('material-icons.css' + ' OK');
-                                }
-                            }
-                        );
-                    }
-                        
-                });
         });
     },
     // -----> Cordova Platform
@@ -255,6 +170,13 @@ module.exports = {
         args = args || [];
         options = options ||{};
         var cmd = `cordova clean ${args.join(' ')}`;
+        exec(cmd);
+    },
+    // -----> Cordova Clean
+    serve: function (args, options) {
+        args = args || [];
+        options = options ||{};
+        var cmd = `cordova serve ${args.join(' ')}`;
         exec(cmd);
     }
 };
