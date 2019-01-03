37c911305adabe52b9aa2d09f0dda9f1788725c7
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-01-03 22:12:11 +0300

    2.0.0-beta.17 release

--- scripts/release.js
+++ scripts/release.js
@@ -1,8 +1,13 @@
 const exec = require('exec-sh');
 const pkg = require('../package.json');
 
-exec('git add .');
-exec(`git commit -m "${pkg.version} release"`);
-exec('git push');
-exec(`git tag v${pkg.version}`);
-exec('npm publish --tag beta');
+async function release() {
+  await exec.promise('git add .');
+  await exec.promise(`git commit -m "${pkg.version} release"`);
+  await exec.promise('git push');
+  await exec.promise(`git tag v${pkg.version}`);
+  await exec.promise('git push origin --tags');
+  await exec.promise('npm publish --tag beta');
+}
+
+release();
