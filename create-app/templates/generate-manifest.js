849c69330396072c30a1014c8f8c2b7ae1c82b08
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-01-30 13:09:53 +0300

    Update workbox to latest
    Fixes #62

--- create-app/templates/generate-manifest.js
+++ create-app/templates/generate-manifest.js
@@ -1,6 +1,6 @@
 module.exports = (options) => {
-  const { name, theming } = options;
-
+  const { name, theming, bundler } = options;
+  const iconsFolder = bundler ? 'static' : 'assets';
   const manifest = {
     name,
     short_name: name,
@@ -12,32 +12,32 @@ module.exports = (options) => {
     theme_color: theming.customColor && theming.color ? `${theming.color}` : '#EE350F',
     icons: [
       {
-        src: '/static/icons/128x128.png',
+        src: `/${iconsFolder}/icons/128x128.png`,
         sizes: '128x128',
         type: 'image/png',
       },
       {
-        src: '/static/icons/144x144.png',
+        src: `/${iconsFolder}/icons/144x144.png`,
         sizes: '144x144',
         type: 'image/png',
       },
       {
-        src: '/static/icons/152x152.png',
+        src: `/${iconsFolder}/icons/152x152.png`,
         sizes: '152x152',
         type: 'image/png',
       },
       {
-        src: '/static/icons/192x192.png',
+        src: `/${iconsFolder}/icons/192x192.png`,
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/static/icons/256x256.png',
+        src: `/${iconsFolder}/icons/256x256.png`,
         sizes: '256x256',
         type: 'image/png',
       },
       {
-        src: '/static/icons/512x512.png',
+        src: `/${iconsFolder}/icons/512x512.png`,
         sizes: '512x512',
         type: 'image/png',
       },
