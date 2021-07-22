f7b99eb3de91c96f13d38ee93a749ef0d20256bf
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-07-22 19:21:53 +0300

    feat: wrap generated custom cordova scripts with `deviceready`

--- create/templates/common/cordova-app.js
+++ create/templates/common/cordova-app.js
@@ -3,7 +3,7 @@ var cordovaApp = {
   /*
   This method hides splashscreen after 2 seconds
   */
-  handleSplashscreen: function() {
+  handleSplashscreen: function () {
     var f7 = cordovaApp.f7;
     if (!window.navigator.splashscreen || f7.device.electron) return;
     setTimeout(() => {
@@ -20,71 +20,75 @@ var cordovaApp = {
     const $ = f7.$;
     if (f7.device.electron) return;
 
-    document.addEventListener('backbutton', function (e) {
-      if ($('.actions-modal.modal-in').length) {
-        f7.actions.close('.actions-modal.modal-in');
-        e.preventDefault();
-        return false;
-      }
-      if ($('.dialog.modal-in').length) {
-        f7.dialog.close('.dialog.modal-in');
-        e.preventDefault();
-        return false;
-      }
-      if ($('.sheet-modal.modal-in').length) {
-        f7.sheet.close('.sheet-modal.modal-in');
-        e.preventDefault();
-        return false;
-      }
-      if ($('.popover.modal-in').length) {
-        f7.popover.close('.popover.modal-in');
-        e.preventDefault();
-        return false;
-      }
-      if ($('.popup.modal-in').length) {
-        if ($('.popup.modal-in>.view').length) {
-          const currentView = f7.views.get('.popup.modal-in>.view');
-          if (currentView && currentView.router && currentView.router.history.length > 1) {
-            currentView.router.back();
-            e.preventDefault();
-            return false;
-          }
+    document.addEventListener(
+      'backbutton',
+      function (e) {
+        if ($('.actions-modal.modal-in').length) {
+          f7.actions.close('.actions-modal.modal-in');
+          e.preventDefault();
+          return false;
         }
-        f7.popup.close('.popup.modal-in');
-        e.preventDefault();
-        return false;
-      }
-      if ($('.login-screen.modal-in').length) {
-        f7.loginScreen.close('.login-screen.modal-in');
-        e.preventDefault();
-        return false;
-      }
+        if ($('.dialog.modal-in').length) {
+          f7.dialog.close('.dialog.modal-in');
+          e.preventDefault();
+          return false;
+        }
+        if ($('.sheet-modal.modal-in').length) {
+          f7.sheet.close('.sheet-modal.modal-in');
+          e.preventDefault();
+          return false;
+        }
+        if ($('.popover.modal-in').length) {
+          f7.popover.close('.popover.modal-in');
+          e.preventDefault();
+          return false;
+        }
+        if ($('.popup.modal-in').length) {
+          if ($('.popup.modal-in>.view').length) {
+            const currentView = f7.views.get('.popup.modal-in>.view');
+            if (currentView && currentView.router && currentView.router.history.length > 1) {
+              currentView.router.back();
+              e.preventDefault();
+              return false;
+            }
+          }
+          f7.popup.close('.popup.modal-in');
+          e.preventDefault();
+          return false;
+        }
+        if ($('.login-screen.modal-in').length) {
+          f7.loginScreen.close('.login-screen.modal-in');
+          e.preventDefault();
+          return false;
+        }
 
-      if ($('.page-current .searchbar-enabled').length) {
-        f7.searchbar.disable('.page-current .searchbar-enabled');
-        e.preventDefault();
-        return false;
-      }
+        if ($('.page-current .searchbar-enabled').length) {
+          f7.searchbar.disable('.page-current .searchbar-enabled');
+          e.preventDefault();
+          return false;
+        }
 
-      if ($('.page-current .card-expandable.card-opened').length) {
-        f7.card.close('.page-current .card-expandable.card-opened');
-        e.preventDefault();
-        return false;
-      }
+        if ($('.page-current .card-expandable.card-opened').length) {
+          f7.card.close('.page-current .card-expandable.card-opened');
+          e.preventDefault();
+          return false;
+        }
 
-      const currentView = f7.views.current;
-      if (currentView && currentView.router && currentView.router.history.length > 1) {
-        currentView.router.back();
-        e.preventDefault();
-        return false;
-      }
+        const currentView = f7.views.current;
+        if (currentView && currentView.router && currentView.router.history.length > 1) {
+          currentView.router.back();
+          e.preventDefault();
+          return false;
+        }
 
-      if ($('.panel.panel-in').length) {
-        f7.panel.close('.panel.panel-in');
-        e.preventDefault();
-        return false;
-      }
-    }, false);
+        if ($('.panel.panel-in').length) {
+          f7.panel.close('.panel.panel-in');
+          e.preventDefault();
+          return false;
+        }
+      },
+      false,
+    );
   },
   /*
   This method does the following:
@@ -121,30 +125,36 @@ var cordovaApp = {
         document.body.style.height = '';
         $('html').removeClass('device-with-keyboard');
       }
-
     });
-    $(document).on('touchstart', 'input, textarea, select', function (e) {
-      var nodeName = e.target.nodeName.toLowerCase();
-      var type = e.target.type;
-      var showForTypes = ['datetime-local', 'time', 'date', 'datetime'];
-      if (nodeName === 'select' || showForTypes.indexOf(type) >= 0) {
-        window.Keyboard.hideFormAccessoryBar(false);
-      } else {
-        window.Keyboard.hideFormAccessoryBar(true);
-      }
-    }, true);
+    $(document).on(
+      'touchstart',
+      'input, textarea, select',
+      function (e) {
+        var nodeName = e.target.nodeName.toLowerCase();
+        var type = e.target.type;
+        var showForTypes = ['datetime-local', 'time', 'date', 'datetime'];
+        if (nodeName === 'select' || showForTypes.indexOf(type) >= 0) {
+          window.Keyboard.hideFormAccessoryBar(false);
+        } else {
+          window.Keyboard.hideFormAccessoryBar(true);
+        }
+      },
+      true,
+    );
   },
   init: function (f7) {
     // Save f7 instance
     cordovaApp.f7 = f7;
 
-    // Handle Android back button
-    cordovaApp.handleAndroidBackButton();
+    document.addEventListener('deviceready', () => {
+      // Handle Android back button
+      cordovaApp.handleAndroidBackButton();
 
-    // Handle Splash Screen
-    cordovaApp.handleSplashscreen();
+      // Handle Splash Screen
+      cordovaApp.handleSplashscreen();
 
-    // Handle Keyboard
-    cordovaApp.handleKeyboard();
+      // Handle Keyboard
+      cordovaApp.handleKeyboard();
+    });
   },
 };
