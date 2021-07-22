b37c6331366604e3b275e7d38774485ee6061096
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-07-22 16:57:19 +0300

    chore: setup prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "env": {
-        "node": true
-    },
-    "rules": {
-        "arrow-body-style": "off",
-        "newline-per-chained-call": "off",
-        "max-len": "off"
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'arrow-body-style': 'off',
+    'newline-per-chained-call': 'off',
+    'max-len': 'off',
+  },
 };
