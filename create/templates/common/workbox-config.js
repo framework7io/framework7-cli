837ada3eda7bb1548997a55a083f0e17b6a25d31
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-07-26 18:06:04 +0300

    feat: add `globIgnores` to workbox config

--- create/templates/common/workbox-config.js
+++ create/templates/common/workbox-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   globDirectory: 'www/',
   globPatterns: ['**/*.{woff,woff2,js,css,png,jpg,svg,html}'],
+  /* pass array of globs to exclude from caching */
+  globIgnores: [],
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
   swDest: 'www/service-worker.js',
 };
