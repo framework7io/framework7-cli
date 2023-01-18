1adcec4df46b7852a7933c8c57783b05f9d5b7cc
Vladimir Kharlampidi <nolimits4web@gmail.com>
2023-01-18 13:11:11 +0300

    fix: update build-cordova script to use new hash format used in Vite

--- create/templates/common/build-cordova.js
+++ create/templates/common/build-cordova.js
@@ -6,14 +6,14 @@ const build = async () => {
   // rebuild JS without modules
   let entry = fs
     .readdirSync(path.resolve(__dirname, '../cordova/www/assets'))
-    .filter((f) => f.includes('index.') && f.includes('.js'))[0];
-  const hash = entry.split('index.')[1].split('.js')[0];
+    .filter((f) => f.includes('index-') && f.includes('.js'))[0];
+  const hash = entry.split('index-')[1].split('.js')[0];
 
   const bundle = await rollup.rollup({
     input: path.resolve(__dirname, '../cordova/www/assets/', entry),
   });
   await bundle.write({
-    file: path.resolve(__dirname, '../cordova/www/assets/', `index.${hash}.js`),
+    file: path.resolve(__dirname, '../cordova/www/assets/', `index-${hash}.js`),
     format: 'iife',
     name: 'MyApp',
     sourcemap: false,
@@ -21,7 +21,7 @@ const build = async () => {
 
   // Remove old chunk files
   fs.readdirSync(path.resolve(__dirname, '../cordova/www/assets')).forEach((f) => {
-    if (f.includes('.js') && f.split('.').length > 2 && f !== `index.${hash}.js`) {
+    if (f.includes('.js') && f.split('.').length > 2 && f !== `index-${hash}.js`) {
       fs.rmSync(path.resolve(__dirname, '../cordova/www/assets', f));
     }
   });
@@ -35,7 +35,7 @@ const build = async () => {
       if (line.includes('<link rel="modulepreload"')) return '';
       if (line.includes('<script type="module"')) return '';
       if (line.includes('</body>'))
-        return `  <script src="assets/index.${hash}.js"></script>\n</body>`;
+        return `  <script src="assets/index-${hash}.js"></script>\n</body>`;
       return line;
     })
     .join('\n');
